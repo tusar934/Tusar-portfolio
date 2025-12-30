e23b75d6d2e85a23c3bc67768c28eabdcd97b6fd
tusar934 <tusarpandaa@gmail.com>
2025-12-30 19:58:25 +0530

    Add hamburger menu functionality to script.js

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ window.addEventListener("scroll", () => {
   document.querySelector(".navbar")
     .classList.toggle("active", window.scrollY > 10);
 });
-  const params = new URLSearchParams(window.location.search);
-  if (params.get("success")) {
-    alert("✅ Message sent successfully!");
-  }
+const params = new URLSearchParams(window.location.search);
+if (params.get("success")) {
+  alert("✅ Message sent successfully!");
+}
+const hamburger = document.querySelector(".hamburger");
+const navlinks = document.querySelector(".navlinks");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navlinks.classList.toggle("active");
+});
